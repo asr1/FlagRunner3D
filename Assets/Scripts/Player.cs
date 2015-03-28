0b95eb1a6b117924693654612ed71746d69f3e6a
asr1 <alexandersrinehart@gmail.com>
2015-03-28 18:50:57 -0500

    I don't remember. Things work.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,10 @@ public class Player : MonoBehaviour {
         transform.localPosition = cell.transform.localPosition;
     }
 
+    public float gravity = 20.0f;
+    public float jumpSpeed = 8.0f;
+    public float speed = 6.0f;
+
     private void Move(MazeDirection direction)
     {
         MazeCellEdge edge = currentCell.GetEdge(direction);
@@ -29,11 +33,56 @@ public class Player : MonoBehaviour {
     }
 
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
+        bool ready = true;
+       // Vector3 MoveDirection = Vector3.zero;
+       // if (GetComponent<CharacterController>().isGrounded)
+        //{
+          //  MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //    MoveDirection = transform.TransformDirection(MoveDirection);
+          //  MoveDirection *= speed;
+ 
+            Vector3 movement = Vector3.zero;
+            movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+       if (ready)
+        {
+            if (movement.x > 0)
+            {
+                Move(currentDirection.GetNextClockWise());
+            } 
+            else if (movement.x < 0)
+            {
+                Move(currentDirection.GetNextCounterclockwise());
+            } 
+            else if (movement.z > 0)
+            {
+                Move(currentDirection);
+            }
+            else if (movement.z < 0)
+            {
+                Move(currentDirection.GetOpposite());
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                rotate(currentDirection.GetNextCounterclockwise());
+            } 
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                rotate(currentDirection.GetNextClockWise());
+            }
+        }
+//            if(Input.GetButton("jump1"))
+//            {
+//                MoveDirection.y = jumpSpeed;
+//            }
+      
+      //  }
+    //    MoveDirection.y -= gravity * Time.deltaTime; //Apply gravity
+       // GetComponent<CharacterController>().Move(MoveDirection * Time.deltaTime);
+       
 
-
-
+        /*
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             Move(currentDirection);
@@ -58,7 +107,7 @@ public class Player : MonoBehaviour {
         {
             rotate(currentDirection.GetNextClockWise());
         }
-        
+        */
     }
 
 }
